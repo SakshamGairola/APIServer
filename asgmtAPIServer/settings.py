896b696e5c18140e15f40e843fad02a8c36ff7ea
Saksham <saksham.gairola28@gmail.com>
2022-09-26 16:32:31 +0530

    Configured Postgresql, Before making migration

--- asgmtAPIServer/settings.py
+++ asgmtAPIServer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'APIServer.apps.ApiserverConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'asgmtAPIServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'indian_banks',
+        'USER': 'postgres',
+        'PASSWORD': 'test',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
